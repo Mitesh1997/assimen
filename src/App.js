8c3bb515d0094859db4e7090d7e0e250da36abf3
Your Name <you@example.com>
2024-04-03 13:12:52 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+ import "./App.css"
 
-function App() {
+export default function App() {
+  const [formData, setFormData] = useState([{ lname: "", lemail: "",lmobile_no:"",lgender:"" }]);
+  const [fdata, setFdata] = useState([
+    { lname: "mife",  lemail: "m@gamil.com",lmobile_no:"877884598",lgender:"male" },
+  ]);
+
+  const addRow = () => {
+    setFormData([...formData, { lname: "", lemail: "", lmobile_no: "", lgender: "" }]);
+  };
+
+  const handleChange = (e, i) => {
+    const { name, value } = e.target;
+    const updatedData = [...formData];
+    updatedData[i][name] = value;
+    setFormData(updatedData);
+  };
+
+  const deleteRow = (i) => {
+    const updatedData = [...formData];
+    updatedData.splice(i, 1);
+    setFormData(updatedData);
+  };
+
+  const submitForm = () => {
+    setFdata([...fdata, ...formData]);
+    setFormData([{ lname: "", lemail: "", lmobile_no: "", lgender: "" }]);
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<table className="customTable">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Gmail</th>
+            <th>Mobile no</th>
+            <th>Gender</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+        {formData.map((val, i) => (
+        <tr key={i}>
+<th>
+          <input
+          type="text"
+
+            name="lname"
+            value={val.lname}
+            onChange={(e) => handleChange(e, i)}
+          />
+          </th>
+          <th>
+          <input
+          type="email"
+            name="lemail"
+            value={val.lemail}
+            onChange={(e) => handleChange(e, i)}
+          />
+          </th>
+          <th>
+          <input
+          type="number"
+            name="lmobile_no"
+            value={val.lmobile_no}
+            onChange={(e) => handleChange(e, i)}
+          />
+          </th> 
+           <th>
+          <input
+          type="text"
+            name="lgender"
+            value={val.lgender}
+            onChange={(e) => handleChange(e, i)}
+          />
+          </th>
+          <th>
+          <button onClick={addRow}>Add</button>
+          <button onClick={() => deleteRow(i)}>Delete</button>
+          </th>
+        </tr>
+      ))}
+        </tbody>
+        </table>
+    
+      <button onClick={submitForm}>Submit</button>
+      <br />
+      <h1>List of above submitted data</h1>
+      <table className="customTable">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>email</th>
+            <th>Mobile no</th>
+            <th>gender</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fdata.map((val, i) => (
+            <tr key={i}>
+              <td>{val.lname}</td>
+              <td>{val.lemail}</td>
+              <td>{val.lmobile_no}</td>
+              <td>{val.lgender}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
-
-export default App;
